dbcd28af840f26ecee03d3ec6fa401e5defdf5fe
Alexandre Victoor <alexvictoor@gmail.com>
2026-08-02 20:57:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service worker de Hawkins Révisions — engendré par scripts/paquet-unique.ts. */
-const CACHE = 'hawkins-95034e9bf9e3'
+const CACHE = 'hawkins-de2fe4eb6e99'
 const RESSOURCES = [
   "./index.html",
   "./manifest.webmanifest",
